e66faad9d1f9c1e029bafb99162e03f45d4cee4b
ArtiDevis <louisayona12@gmail.com>
2026-03-24 17:51:15 +0100

    Update send-email.js

--- netlify/functions/send-email.js
+++ netlify/functions/send-email.js
@@ -1,1 +1,39 @@
+exports.handler = async (event) => {
+  if (event.httpMethod !== 'POST') {
+    return { statusCode: 405, body: 'Method not allowed' };
+  }
 
+  const RESEND_KEY = process.env.RESEND_KEY;
+
+  try {
+    const { to, subject, html } = JSON.parse(event.body);
+
+    const res = await fetch('https://api.resend.com/emails', {
+      method: 'POST',
+      headers: {
+        'Authorization': `Bearer ${RESEND_KEY}`,
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        from: 'ArtiDevis <onboarding@resend.dev>',
+        to: [to],
+        subject,
+        html
+      })
+    });
+
+    const data = await res.json();
+
+    return {
+      statusCode: 200,
+      headers: { 'Access-Control-Allow-Origin': '*' },
+      body: JSON.stringify(data)
+    };
+  } catch (e) {
+    return {
+      statusCode: 500,
+      headers: { 'Access-Control-Allow-Origin': '*' },
+      body: JSON.stringify({ error: e.message })
+    };
+  }
+};
